4922cc07b9331019d9e78260779870b2c99170c9
Ohsori001106 <surnameMin@Gmail.com>
2024-04-01 17:43:32 +0900

    덤버라 세상아

---
+++ Assets/02.Scripts/UI/UI_CharacterStat.cs
@@ -0,0 +1,31 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_CharacterStat : MonoBehaviour
+{
+    public static UI_CharacterStat Instance { get; private set; }
+    public Character MyCharacter;
+   
+    [Header("체력 슬라이더 UI")]
+    public Slider HealthSliderUI;
+    [Header("스태미나 슬라이더 UI")]
+    public Slider StaminaSliderUI;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Update()
+    {
+        if(MyCharacter == null)
+        {
+            return;
+        }
+        HealthSliderUI.value = (float)MyCharacter.Stat.Health / MyCharacter.Stat.MaxHealth;
+        StaminaSliderUI.value = MyCharacter.Stat.Stamina / MyCharacter.Stat.MaxStamina;
+    }
+}
